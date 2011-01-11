aa368c2c873b853b1ea9a49e883a828fee7e9f08
Mark Kahn <cwolves@gmail.com>
2011-01-11 13:43:46 -0800

    Removing old admin directory

--- __admin/js/modules/users.js
+++
@@ -1,9 +0,0 @@
-modules.Users = {
-	requiredAPI : 'user/add',
-	name        : 'Users',
-	template    : '#{name}',
-	searchAPI   : 'user/getList',
-	addAPI      : 'user/add',
-	render      : function(key){
-	}
-}
